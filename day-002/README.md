aa8ce598ae366b3c3863b4ceea38a433faee8670
Mustafa Zaki Assagaf <mustafa.segf@gmail.com>
2025-04-04 00:26:21 +0700

    feat: added day 002 for grayscale and from pmpp chap 3

---
+++ day-002/README.md
@@ -0,0 +1,3 @@
+# Day 2
+
+File: [matmul.cu](https://github.com/mustafasegf/cuda-100-days-challange/blob/master/day-002/matmul.cu)
--- generate.sh
+++ generate.sh
@@ -1,11 +1,60 @@
 #!/usr/bin/env bash
 
+# Read and increment day
 last_day=$(<.lastday)
 day=$((last_day + 1))
-
 padded=$(printf "%03d" "$day")
 
-mkdir -p "day-${padded}"
+# Ask for program name if not given
+if [ -z "$1" ]; then
+  # read -p "Enter program name (without .cu): " prog
+  prog="main"
+else
+  prog="$1"
+fi
 
+folder="day-${padded}"
+mkdir -p "$folder"
+
+# Create Makefile
+cat > "$folder/Makefile" <<EOF
+NVCC = nvcc
+NVCCFLAGS = -std=c++11 -O3
+TARGET = ${prog}
+SOURCE = ${prog}.cu
+
+all: \$(TARGET)
+
+\$(TARGET): \$(SOURCE)
+	\$(NVCC) \$(NVCCFLAGS) -o \$@ \$<
+
+clean:
+	rm -f \$(TARGET)
+
+run:
+	./\$(TARGET)
+
+.PHONY: all clean run
+EOF
+
+# Create README.md
+cat > "$folder/README.md" <<EOF
+# Day ${day}
+
+File: [${prog}.cu](https://github.com/mustafasegf/cuda-100-days-challange/blob/master/${folder}/${prog}.cu)
+EOF
+
+
+# Create .gitignore
+cat > "$folder/.gitignore" <<EOF
+${prog}
+EOF
+
+# Create empty .cu file
+touch "$folder/${prog}.cu"
+
+# Update .lastday
 echo "$day" > .lastday
+
+echo "Created $folder with $prog.cu, Makefile, .gitignore and README.md"
 
